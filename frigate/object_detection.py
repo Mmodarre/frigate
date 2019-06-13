b23e156195575e61f234b97b4f6c9280904b5639
root <modarressi.mehdi@gmail.com>
2019-06-13 01:51:56 +0000

    working for MYRIAD

--- frigate/object_detection.py
+++ frigate/object_detection.py
@@ -49,8 +49,9 @@ class PreppedQueueProcessor(threading.Thread):
         self.input_blob = next(iter(self.net.inputs))
         self.out_blob = next(iter(self.net.outputs))
         self.exec_net = self.plugin.load(network=self.net, num_requests=2)
-        #n, c, h, w = net.inputs[self.input_blob].shape
-        del net
+        self.n, self.c, self.h, self.w = self.net.inputs[self.input_blob].shape
+        #print(self.n,self.c,self.h,self.w)
+        del self.net
         with open(labels, 'r') as f:
             self.labels_map = [x.strip() for x in f]
         self.next_request_id = 1
@@ -60,34 +61,37 @@ class PreppedQueueProcessor(threading.Thread):
         # process queue...
         while True:
             frame = self.prepped_frame_queue.get()
-
+            #print(frame.type())
             # Actual detection.
             #objects = self.engine.DetectWithInputTensor(frame['frame'], threshold=frame['region_threshold'], top_k=3)
             inf_start = time.time()
-            self.exec_net.start_async(request_id=next_request_id, inputs={input_blob: frame})
-            if self.exec_net.requests[cur_request_id].wait(-1) == 0 :
+            in_frame = frame['frame']
+            #in_frame = in_frame.reshape((self.n, self.c, self.h, self.w))
+            self.exec_net.start_async(request_id=int(self.cur_request_id), inputs={self.input_blob: in_frame})
+            parsed_objects = []
+            if self.exec_net.requests[self.cur_request_id].wait(-1) == 0 :
                 inf_end = time.time()
                 det_time = inf_end - inf_start
 
             # Parse detection results of the current request
-                res = self.exec_net.requests[cur_request_id].outputs[self.out_blob]
+                res = self.exec_net.requests[self.cur_request_id].outputs[self.out_blob]
             # parse and pass detected objects back to the camera
-            parsed_objects = []
 
-            for obj in res[0][0]:
+                for obj in res[0][0]:
                 # Add objects when probability more than specified threshold
-                if obj[2] > frame['region_threshold'] :
-                    class_id = int(obj[1])
-                    det_label = labels_map[class_id] if labels_map else str(class_id)
-                    parsed_objects.append({
-                        'frame_time': frame['frame_time'],
-                        'name': str(det_label),
-                        'score': float(obj[2]),
-                        'xmin' : int((obj[3] * frame['region_size']) + frame['region_x_offset']),
-                        'ymin' : int((obj[4] * frame['region_size']) + frame['region_x_offset']),
-                        'xmax' : int((obj[5] * frame['region_size']) + frame['region_x_offset']),
-                        'ymax' : int((obj[6] * frame['region_size']) + frame['region_x_offset'])
-                    })
+                    if obj[2] > frame['region_threshold'] :
+                        #print(obj[1])
+                        class_id = int(obj[1])
+                        det_label = self.labels_map[class_id] if self.labels_map else str(class_id)
+                        parsed_objects.append({
+                            'frame_time': frame['frame_time'],
+                            'name': str(det_label),
+                            'score': float(obj[2]),
+                            'xmin' : int((obj[3] * frame['region_size']) + frame['region_x_offset']),
+                            'ymin' : int((obj[4] * frame['region_size']) + frame['region_x_offset']),
+                            'xmax' : int((obj[5] * frame['region_size']) + frame['region_x_offset']),
+                            'ymax' : int((obj[6] * frame['region_size']) + frame['region_x_offset'])
+                        })
                 
 #            for obj in objects:
 #                box = obj.bounding_box.flatten().tolist()
@@ -139,24 +143,27 @@ class FramePrepper(threading.Thread):
                 frame_time = self.frame_time.value
             
             # convert to RGB
-            cropped_frame_rgb = cv2.cvtColor(cropped_frame, cv2.COLOR_BGR2RGB)
-            cropped_frame_rgb= cropped_frame_rgb.transpose((2, 0, 1))
+            #cropped_frame_rgb = cv2.cvtColor(cropped_frame, cv2.COLOR_BGR2RGB)
+            #cropped_frame_rgb= cropped_frame_rgb.transpose((2, 0, 1))
             # Resize to 300x300 if needed
-            if cropped_frame_rgb.shape != (300, 300, 3):
-                cropped_frame_rgb = cv2.resize(cropped_frame_rgb, dsize=(300, 300), interpolation=cv2.INTER_LINEAR)
+            #if cropped_frame_rgb.shape != (300, 300, 3):
+            #    cropped_frame_rgb = cv2.resize(cropped_frame_rgb, dsize=(300, 300), interpolation=cv2.INTER_LINEAR)
             # Expand dimensions since the model expects images to have shape: [1, 300, 300, 3]
-            frame_expanded = np.expand_dims(cropped_frame_rgb, axis=0)
+            #frame_expanded = np.expand_dims(cropped_frame_rgb, axis=0)
+            frame_expanded = cv2.resize(cropped_frame, (300, 300))
+            frame_expanded = frame_expanded.transpose((2, 0, 1))  # Change data layout from HWC to CHW
+            frame_expanded = frame_expanded.reshape((1, 3, 300, 300))
 
             # add the frame to the queue
             if not self.prepped_frame_queue.full():
                 self.prepped_frame_queue.put({
                     'camera_name': self.camera_name,
                     'frame_time': frame_time,
-                    'frame': frame_expanded.flatten().copy(),
+                    'frame': frame_expanded.copy(),
                     'region_size': self.region_size,
                     'region_threshold': self.region_threshold,
                     'region_x_offset': self.region_x_offset,
                     'region_y_offset': self.region_y_offset
                 })
-            else:
-                print("queue full. moving on")
+            #else:
+            #    print("queue full. moving on")
